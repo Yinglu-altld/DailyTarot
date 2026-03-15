d6c5c558385a344fa4c3f4b83de7a4499c7f3fe7
Yinglu-altld <altld233@gmail.com>
2026-03-15 16:47:44 +0100

    Initial Commit

---
+++ DailyTarot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyTarot
+//
+//  Created by 卢颖 on 2026/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyTarot/DailyTarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTarotApp.swift
+//  DailyTarot
+//
+//  Created by 卢颖 on 2026/3/15.
+//
+
+import SwiftUI
+
+@main
+struct DailyTarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
